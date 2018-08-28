e2b2e7107699994c8629fb6632ca69532343e843
Mary Rys <rys.mary@gmail.com>
2018-08-27 21:16:16 -0500

    added object challenge

---
+++ objectchallenge.js
@@ -0,0 +1,26 @@
+let elizabethSanger = {
+
+  congressionalDistrict: 5,
+
+  statements: [ 
+      {statement: "I love taxes", category: "taxes"},
+      {statement: "I love working", category: "jobs"},
+  ],
+  donationFormUrl: 'www.google.com',
+  events: [
+      {date: 08/27/2018, title: "super cool party", description:"Come to party"}
+  ],
+  volunteers: [
+      {name: "Callan", address: "500 interstate blvd S", email: "email@example.com", phoneNumber: "123-456-7890"},
+  ],
+  biography: "I'm so cool!  please elect me",
+  images: [
+      {
+      imageUrl: "",
+      description: "good image",
+      type: "headshot",
+      },
+  ],
+  missionStatement: "Do good stuff",
+  voterRegistrationUrl: 'www.google.com'
+};
